df14f54898e1d96d2267f9c167f5fea17b6770a9
12090113 <47487266+12090113@users.noreply.github.com>
2022-10-17 18:01:32 -0700

    Added direction setting to OrbitPlacer
    Allowing placing orbits clockwise or counterclockwise.
    OrbitPlacer now only runs when the body is moved, improving performance in the editor. (However that means you have to wiggle the planet after turning OrbitPlacer on for it to run; I'll fix that later)

--- Marching Squares/Assets/Scripts/Edit/OrbitPlacer.cs
+++ Marching Squares/Assets/Scripts/Edit/OrbitPlacer.cs
@@ -8,8 +8,15 @@ public class OrbitPlacer : MonoBehaviour
     public GravityObject gravObj;
     public GravityObject centralBody;
     public float massOffset = 0;
+    public Direction direction = Direction.Clockwise;
     public bool orbit = true;
-    // Start is called before the first frame update
+    
+    public enum Direction
+    {
+        Clockwise = 1,
+        Counterclockwise = -1
+    }
+
     void Start()
     {
         if (Application.isPlaying)
@@ -23,8 +30,9 @@ public class OrbitPlacer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (orbit)
+        if (orbit && transform.hasChanged)
             Orbit();
+        transform.hasChanged = false;
     }
 
     void Orbit()
@@ -47,7 +55,7 @@ public class OrbitPlacer : MonoBehaviour
         gravObj = GetComponent<GravityObject>();
         Vector3 pos1 = gravObj.transform.position;
         Vector3 pos2 = centralBody.transform.position;
-        Vector3 dir = Vector2.Perpendicular(pos1 - pos2).normalized;
+        Vector3 dir = Vector2.Perpendicular(pos2 - pos1).normalized * (int)direction;
         gravObj.velocity = Mathf.Sqrt(NBodySimulation.G * (centralBody.mass + massOffset) / Vector3.Distance(pos1, pos2)) * dir + centralBody.velocity;
     }
 }
